129a0a1f527601a2cea9bfe8ddc47ededc783772
Igor Bondarev <garkol@bk.ru>
2023-04-13 22:32:06 +0300

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+
+        tree.add(7);tree.add(3);tree.add(9);tree.add(2);tree.add(1);tree.add(5);tree.add(4);tree.add(6);tree.add(8);
+        tree.add(16);tree.add(0);tree.add(30);tree.add(50);tree.add(25);
+
+        System.out.println("1. Реализовать поиск максимального элемента в дереве.");
+        Tree.findFirstElement(tree);
+        Tree.findLastElement(tree);
+        System.out.println("<====================================>\n");
+        System.out.println("2. Реализовать подсчет листьев.");
+        System.out.println("В данном дереве " + Tree.getChildrenCount(tree) + " листьев.");
+
+
+    }
+
+}
